7fb8b46c2ae066a08555d33c892990d5ea9f2d3e
Ivan <mayhemk57@gmail.com>
2025-04-14 16:35:55 -0600

    Check winner, add score, restart game!

--- Program.cs
+++ Program.cs
@@ -1,15 +1,22 @@
 ﻿internal class Program
 {
+  static int PlayerWins = 0;
+  static int ComputerWins = 0;
   private static void Main()
   {
+    Console.Clear();
     Console.WriteLine("Howdily-doodily, neighborino!");
+    Console.WriteLine($"Current Score: Player - {PlayerWins}, Computer - {ComputerWins}");
+
     string userHand = chooseHand();
     string computerHand = getComputerHand();
     Console.WriteLine($"You chose: {userHand}");
     Console.WriteLine($"Computer Chose: {computerHand}");
     string result = DetermineWinner(userHand, computerHand);
     Console.WriteLine(result);
+    Console.WriteLine($"Player Wins: {PlayerWins}, Computer Wins: {ComputerWins}");
 
+    PlayAgain();
 
     static string chooseHand()
     {
@@ -69,11 +76,29 @@
           (userHand == "Scissors" && computerHand == "Paper")
       )
       {
+        PlayerWins++;
         return "You Win!";
       }
       else
       {
+        ComputerWins++;
         return "You Lose!";
+      }
+    }
+
+    static void PlayAgain()
+    {
+      Console.WriteLine("Play again? (y/n)");
+      string playAgain = Console.ReadLine().ToLower();
+
+      if (playAgain == "y")
+      {
+        Main();
+      }
+      else
+      {
+        Console.WriteLine("Thanks for playing!");
+        Environment.Exit(0);
       }
     }
   }
--- obj/Debug/net9.0/rock_paper_scissors.AssemblyInfo.cs
+++ obj/Debug/net9.0/rock_paper_scissors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rock_paper_scissors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47283307c71018e2c032c9c129aa19a066fe8085")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b761d1e01601f70cee1cb82963089c718977eda")]
 [assembly: System.Reflection.AssemblyProductAttribute("rock_paper_scissors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rock_paper_scissors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
