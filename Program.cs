8b761d1e01601f70cee1cb82963089c718977eda
Ivan <mayhemk57@gmail.com>
2025-04-14 16:20:58 -0600

    now it checks for winner!

--- Program.cs
+++ Program.cs
@@ -3,28 +3,78 @@
   private static void Main()
   {
     Console.WriteLine("Howdily-doodily, neighborino!");
-    string userHand = ChooseHand();
+    string userHand = chooseHand();
+    string computerHand = getComputerHand();
     Console.WriteLine($"You chose: {userHand}");
-  }
-  static string ChooseHand()
-  {
-    Console.WriteLine("Choose A Hand");
-    Console.WriteLine("1. Rock");
-    Console.WriteLine("2. Paper");
-    Console.WriteLine("3. Scissors");
-    string userInput = Console.ReadLine();
+    Console.WriteLine($"Computer Chose: {computerHand}");
+    string result = DetermineWinner(userHand, computerHand);
+    Console.WriteLine(result);
 
-    switch (userInput)
+
+    static string chooseHand()
     {
-      case "1":
-        return "Rock";
-      case "2":
-        return "Paper";
-      case "3":
-        return "Scissors";
-      default:
-        Console.WriteLine("Invalid Choice, Please select 1, 2, or 3");
-        return ChooseHand();
+      Console.WriteLine("Choose A Hand");
+      Console.WriteLine("1. Rock");
+      Console.WriteLine("2. Paper");
+      Console.WriteLine("3. Scissors");
+      string userInput = Console.ReadLine();
+
+      switch (userInput)
+      {
+        case "1":
+          return "Rock";
+        case "2":
+          return "Paper";
+        case "3":
+          return "Scissors";
+        default:
+          Console.WriteLine("Invalid Choice, Please select 1, 2, or 3");
+          return chooseHand();
+      }
+    }
+
+
+    static string getComputerHand()
+    {
+      int randomNumber = new Random().Next(1, 4);
+      string computerHand;
+
+      switch (randomNumber)
+      {
+        case 1:
+          computerHand = "Rock";
+          break;
+        case 2:
+          computerHand = "Paper";
+          break;
+        case 3:
+          computerHand = "Scissors";
+          break;
+        default:
+          Console.WriteLine("Random Number Fail");
+          return getComputerHand();
+      }
+      return computerHand;
+    }
+
+    static string DetermineWinner(string userHand, string computerHand)
+    {
+      if (userHand == computerHand)
+      {
+        return "It's a tie!";
+      }
+      else if (
+          (userHand == "Rock" && computerHand == "Scissors") ||
+          (userHand == "Paper" && computerHand == "Rock") ||
+          (userHand == "Scissors" && computerHand == "Paper")
+      )
+      {
+        return "You Win!";
+      }
+      else
+      {
+        return "You Lose!";
+      }
     }
   }
 }
--- obj/Debug/net9.0/rock_paper_scissors.AssemblyInfo.cs
+++ obj/Debug/net9.0/rock_paper_scissors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rock_paper_scissors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c0418d21a2c636714dd8d9aec657733f29ee078")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47283307c71018e2c032c9c129aa19a066fe8085")]
 [assembly: System.Reflection.AssemblyProductAttribute("rock_paper_scissors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rock_paper_scissors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
